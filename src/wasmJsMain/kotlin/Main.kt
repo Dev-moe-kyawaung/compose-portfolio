f439a68b2b8134c4ececd12372f0c502bc6adf37
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-07-11 21:11:46 +0630

    Create Main.kt

---
+++ src/wasmJsMain/kotlin/Main.kt
@@ -0,0 +1,10 @@
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.CanvasBasedWindow
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    CanvasBasedWindow(canvasElementId = "ComposeTarget", title = "Moe Kyaw Aung — Senior Android Developer") {
+        App()
+    }
+}
+
