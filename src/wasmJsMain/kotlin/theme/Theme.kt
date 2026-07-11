1c294a59ca3a2aff9de50ebf622519577277956e
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-07-11 21:13:38 +0630

    Create Theme.kt

---
+++ src/wasmJsMain/kotlin/theme/Theme.kt
@@ -0,0 +1,22 @@
+package theme
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    // Dark theme colors
+    val BackgroundPrimary = Color(0xFF0D0D0D)
+    val BackgroundSecondary = Color(0xFF16213E)
+    val BackgroundTertiary = Color(0xFF1A1A2E)
+    val AccentGold = Color(0xFFC9A84C)
+    val TextPrimary = Color(0xFFF5F5F5)
+    val TextSecondary = Color(0xFFA0A0A0)
+    val BorderSubtle = Color(0x26C9A84C)
+    
+    // Light theme colors
+    val LightBackgroundPrimary = Color(0xFFFAFAF8)
+    val LightBackgroundSecondary = Color(0xFFFFFFFF)
+    val LightBackgroundTertiary = Color(0xFFF0EFEA)
+    val LightTextPrimary = Color(0xFF16213E)
+    val LightTextSecondary = Color(0xFF4A4A4A)
+}
+
